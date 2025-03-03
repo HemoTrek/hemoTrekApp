c060544455a37bc2639914b9fff4adb8b7891779
SeanKRose <77079031+SeanKRose@users.noreply.github.com>
2025-03-03 13:27:29 -0500

    Added cleaning instructions scrollbar with checkboxes and images

--- cleaningScreen/cleaningScreen.py
+++ cleaningScreen/cleaningScreen.py
@@ -1,4 +1,57 @@
 from helperPage.helperPage import helperPage
+from kivy.app import App
+from kivy.uix.screenmanager import Screen
+from kivy.uix.boxlayout import BoxLayout
+from kivymd.uix.selectioncontrol import MDCheckbox
+from kivymd.uix.label import MDLabel
+from kivymd.uix.scrollview import MDScrollView
+from kivy.uix.image import Image
+from kivymd.uix.boxlayout import MDBoxLayout
 
-class CleaningScreen(helperPage):
-    pass
+class CleaningScreen(Screen):
+
+    def on_pre_enter(self, *args):
+        """Populate the cleaning instructions dynamically."""
+        setup_steps = [
+            "Open the door",
+            "Remove Capillary tubes",
+            "Remove the pipette tip",
+            "Remove both microcentrifuge tubes",
+            "Dispose of all products in their appropriate waste receptacles",
+        ]
+
+        self.ids.setup_steps.clear_widgets()
+
+        for index, step in enumerate(setup_steps, start=1):
+            # Create a horizontal box for step description and checkbox
+            step_layout = MDBoxLayout(orientation="horizontal", size_hint_y=None, height="50dp", md_bg_color = (1,1,1,1))
+
+            # Step description (75% of width)
+            step_text = MDLabel(text=step, size_hint_x=0.75, halign="left", theme_text_color="Custom", text_color=(0,0,0,1))
+            step_layout.add_widget(step_text)
+
+            # Checkbox (25% of width)
+            checkbox = MDCheckbox(size_hint_x=0.25, pos_hint={"center_y": 0.5}, color_active=(0, 0, 0, 1))
+            step_layout.add_widget(checkbox)
+
+            # Add the step layout to the list
+            self.ids.setup_steps.add_widget(step_layout)
+
+            # Add the corresponding image below
+            image = Image(source=f"icons/cleaningImage{index}.png", size_hint_y=None, height="150dp")
+            self.ids.setup_steps.add_widget(image)
+   
+    def return_home(self):
+        """
+        Navigates back to the home screen.
+        """
+        self.manager.current = 'home'
+
+    def start_test(self, *args):
+        """
+        This method is called when the 'Start Test' button is pressed.
+        """
+        print("listPatientsScreen - Test Started")
+        print(*args)
+
+        self.manager.current = 'test'        
